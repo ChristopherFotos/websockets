0c4cff3cf5e69fc04dcd8f32126bda73a866ff60
Christopher Fotos <christopherfotos@Christophers-MacBook-Pro.local>
2024-06-23 22:41:02 -0400

    put everything in one file

--- server/index.js
+++ server/index.js
@@ -1,3 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+resolve = require("path").resolve;
+
+const app = express();
+app.use(cors());
+app.use(express.static(resolve("../client")));
+
+app.get("/home", (req, res) => {
+  res.sendFile(resolve("../client/index.html"));
+});
+
+app.listen(3030, () => console.log("Server started on port" + "3030"));
+
 const http = require("http").createServer();
 const io = require("socket.io")(http, {
   cors: { origin: "*" },
