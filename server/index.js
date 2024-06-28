a51c3cfd5aadb1ef7a902d5b626d7d95f77d4f1b
Christopher Fotos <christopherfotos@Christophers-MacBook-Pro.local>
2024-06-27 21:20:10 -0400

    create server directory

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const cors = require("cors");
 resolve = require("path").resolve;
 
-const app = express();
-app.use(cors());
-app.use(express.static(resolve("../client")));
+// const app = express();
+// app.use(cors());
+// app.use(express.static(resolve("../client")));
 
-app.get("/home", (req, res) => {
-  res.sendFile(resolve("../client/index.html"));
-});
+// app.get("/home", (req, res) => {
+//   res.sendFile(resolve("../client/index.html"));
+// });
 
-app.listen(3030, () => console.log("Server started on port" + "3030"));
+// app.listen(3030, () => console.log("Server started on port" + "3030"));
 
 const http = require("http").createServer();
 const io = require("socket.io")(http, {
