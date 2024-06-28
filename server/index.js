c401a95f2167caf78cae16689c6a479906557143
Christopher Fotos <christopherfotos@Christophers-MacBook-Pro.local>
2024-06-27 21:59:39 -0400

    add cors

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 resolve = require("path").resolve;
 
-// const app = express();
-// app.use(cors());
-// app.use(express.static(resolve("../client")));
+const app = express();
+app.use(cors());
+app.use(express.static(resolve("../client")));
 
 // app.get("/home", (req, res) => {
 //   res.sendFile(resolve("../client/index.html"));
@@ -12,8 +12,33 @@ resolve = require("path").resolve;
 
 // app.listen(3030, () => console.log("Server started on port" + "3030"));
 
-const http = require("http").createServer();
-const io = require("socket.io")(http, {
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*" /* @dev First, read about security */,
+    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    "Access-Control-Max-Age": 2592000, // 30 days
+    /** add other headers as per requirement */
+  };
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+
+  if (["GET", "POST"].indexOf(req.method) > -1) {
+    res.writeHead(200, headers);
+    res.end("Hello World");
+    return;
+  }
+
+  res.writeHead(405, headers);
+  res.end(`${req.method} is not allowed for the request.`);
+});
+
+const io = require("socket.io")(server, {
   cors: { origin: "*" },
 });
 
@@ -28,4 +53,4 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(8080, () => console.log("listening on 8080"));
+server.listen(8080, () => console.log("listening on 8080"));
